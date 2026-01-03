81e5f531b819d9e62a582c112e5bc011d1191d86
JMiguelFL <juanfaoussi@gmail.com>
2026-01-04 00:07:33 +0100

    Refactor: extraer lógica de validación dde input a función independiente

--- juego.py
+++ juego.py
@@ -1,38 +1,42 @@
 import random
-from colorama import init, Fore, Style 
+from colorama import init, Fore, Style
 
 init(autoreset=True)
 
+def pedir_numero():
+    while True:
+        try:
+            entrada = input(Fore.YELLOW + "Introduce tu número: ")
+            return int(entrada)
+        except ValueError:
+            print(Fore.RED + "Error: Por favor escribe solo números.")
+
 def adivina_numero():
-    print(Fore.CYAN + "Bienvenido. Estoy pensando en un número del 1 al 100...")
-    
+    print(Fore.CYAN + "Bienvenido, estoy pensando un número del 1 al 100")
+
     numero_secreto = random.randint(1, 100)
     intentos = 0
     ganaste = False
     historial = []
 
     while not ganaste:
-        try:
-            entrada = input(Fore.YELLOW + "Introduce tu número: ")
-            numero_usuario = int(entrada)
-            historial.append(numero_usuario)
-            intentos += 1
+        numero_usuario = pedir_numero()
 
-            if numero_usuario < numero_secreto:
-                print(Fore.BLUE + "Más alto")
-            elif numero_usuario > numero_secreto:
-                print(Fore.BLUE + "Más bajo")
-            else:
-                print(Fore.GREEN + Style.BRIGHT + f"¡Felicidades! Lo adivinaste en {intentos} intentos.")
-                ganaste = True
-                
-        except ValueError:
-            print(Fore.RED + "Error: Por favor, escribe solo números.")
+        historial.append(numero_usuario)
+        intentos += 1
 
+        if numero_usuario < numero_secreto:
+            print(Fore.BLUE + "Más alto")
+        elif numero_usuario > numero_secreto:
+            print(Fore.BLUE + "Más bajo")
+        else:
+            print(Fore.GREEN + "Has acertado el número")
+            ganaste = True
+    
     print(Style.BRIGHT + "\n--- Resumen de tu partida ---")
     for numero in historial:
         if numero == numero_secreto:
-            print(Fore.GREEN + f"- {numero} (¡Correcto!)")
+            print(Fore.GREEN + f"- {numero} (Correcto)")
         else:
             print(Fore.RED + f"- {numero}")
 
